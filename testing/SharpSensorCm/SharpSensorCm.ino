230f64aed732b17da63c81d1714f202edeea7e1f
nickdinardo <nickdinardo@alice.it>
2017-05-01 08:11:50 +0200

    prova

--- testing/SharpSensorCm/SharpSensorCm.ino
+++ testing/SharpSensorCm/SharpSensorCm.ino
@@ -1,6 +1,7 @@
 #include <SharpIR.h>
 
 #define ir A0
+
 #define model 1080
 // ir: the pin where your sensor is attached
 // model: an int that determines your sensor:  1080 for GP2Y0A21Y
@@ -8,6 +9,7 @@
 //                                            (working distance range according to the datasheets)
 
 SharpIR SharpIR(ir, model);
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,6 +26,8 @@ void loop() {
 
   Serial.print("Mean distance: ");  // returns it to the serial monitor
   Serial.println(dis);
+
+  
 
   unsigned long pepe2 = millis() - pepe1; // the following gives you the time taken to get the measurement
   // Serial.print("Time taken (ms): ");
